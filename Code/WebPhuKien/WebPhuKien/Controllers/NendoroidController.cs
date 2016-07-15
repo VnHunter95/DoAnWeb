ac41274533730da4e96059b51141af4d4cb61c50
dragonfist31 <tranquockhai1995@yahoo.com.vn>
2016-07-15 23:14:53 +0700

    Edit Code
    Chả edit được bao nhiêu :'((

---
+++ Code/WebPhuKien/WebPhuKien/Controllers/NendoroidController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebPhuKien.Controllers
+{
+    public class NendoroidController : Controller
+    {
+        //
+        // GET: /Nendoroid/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
