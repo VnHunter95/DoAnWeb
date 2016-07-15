ce4e25fb47d7995379fc0ae81971cfe93dde6320
Trần Vĩ Toàn <tranvytoan@gmail.com>
2016-07-15 09:14:50 +0700

    Add code

---
+++ Code/WebPhuKien/WebPhuKien/Controllers/PhuKienController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebPhuKien.Controllers
+{
+    public class PhuKienController : Controller
+    {
+        //
+        // GET: /PhuKien/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ Code/WebPhuKien/WebPhuKien/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPhuKien.Startup))]
+namespace WebPhuKien
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
