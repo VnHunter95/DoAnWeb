1f543f546013047881b7e7501a6c798672990ee0
Trần Vĩ Toàn <tranvytoan@gmail.com>
2016-07-23 13:26:33 +0700

    Update Cái Banner
    +Add Banner controller
    +Add Banner/Index.cshtml
    +Fix code LayoutPageUser: Thêm Paritcal View banner.

---
+++ Code/WebPhuKien/WebPhuKien/Controllers/BannerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPhuKien.Models;
+
+namespace WebPhuKien.Controllers
+{
+    public class BannerController : Controller
+    {
+        //
+        // GET: /Banner/
+        DataClasses1DataContext data = new DataClasses1DataContext();
+        public ActionResult Index()
+        {
+            var kq = from q in data.Banners
+                     select q; 
+            return PartialView(kq);
+        }
+	}
+}
