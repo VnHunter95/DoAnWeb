950862b9c88e035dbef61532a79fc3d6f14d4349
Trần Vĩ Toàn <tranvytoan@gmail.com>
2016-08-01 12:36:26 +0700

    UPdate Admin
    +Dang nhap ADmin
    +Layout Admin
    +Admin Controller

---
+++ Code/WebPhuKien/WebPhuKien/Controllers/AdminController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPhuKien.Models;
+namespace WebPhuKien.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+        DataClasses1DataContext data = new DataClasses1DataContext();
+          
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login( FormCollection collection)
+        {
+            var tendn = collection["username"];
+            var mk = collection["password"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải Nhập Tên Đăng Nhập";
+            }
+            else if (String.IsNullOrEmpty(mk))
+            {
+                ViewData["Loi2"] = "Phải Nhập Mật Khẩu";
+            }
+            else
+            {
+                QUANTRI ad = data.QUANTRIs.SingleOrDefault(n => n.User == tendn && n.Password == mk);
+                if (ad != null)
+                {
+                    Session["Taikhoanadmin"] = ad;
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                {
+                    ViewBag.Thongbao = "Đăng Nhập Không Thành Công";
+                }
+            }
+            return this.Login();
+
+        }
+	}
+}
