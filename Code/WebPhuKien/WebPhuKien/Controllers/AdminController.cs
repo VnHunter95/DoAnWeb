a87bac5bbc067fdc91b6aa30c296dff3fd6890c0
Trần Vĩ Toàn <tranvytoan@gmail.com>
2016-08-03 11:58:13 +0700

    Update #1

--- Code/WebPhuKien/WebPhuKien/Controllers/AdminController.cs
+++ Code/WebPhuKien/WebPhuKien/Controllers/AdminController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebPhuKien.Models;
+using PagedList;
+using PagedList.Mvc;
+using System.IO;
 namespace WebPhuKien.Controllers
 {
     public class AdminController : Controller
@@ -17,7 +20,36 @@ namespace WebPhuKien.Controllers
             return View();
         }
 
-
+        public ActionResult Sanpham(int ?page)
+        {
+            int pageNum = (page ?? 1);
+            int pageSize = 7;
+            return View(data.SANPHAMs.ToList().OrderBy(n=>n.Idsp).ToPagedList(pageNum,pageSize));
+        }
+        [HttpGet]
+        public ActionResult Themsanpham()
+        {
+            ViewBag.IdLoai = new SelectList(data.LOAISANPHAMs.ToList().OrderBy(n => n.Tenloai), "Idloai", "Tenloai");
+            ViewBag.IdNsx = new SelectList(data.NHASANXUATs.ToList().OrderBy(n => n.Tennsx), "Idnsx", "Tennsx");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Themsanpham(SANPHAM sp, HttpPostedFileBase fileupload)
+        {
+            var fileName = Path.GetFileName(fileupload.FileName);
+            var path = Path.Combine(Server.MapPath("~/Hinhsanpham"), fileName);
+            if (System.IO.File.Exists(path))
+            {
+                ViewBag.Thongbao = "Hình ảnh đã tồn tại";
+            }
+            else 
+            {
+                fileupload.SaveAs(path);
+            }
+            ViewBag.IdLoai = new SelectList(data.LOAISANPHAMs.ToList().OrderBy(n => n.Tenloai), "Idloai", "Tenloai");
+            ViewBag.IdNsx = new SelectList(data.NHASANXUATs.ToList().OrderBy(n => n.Tennsx), "Idnsx", "Tennsx");
+            return View();
+        }
         [HttpGet]
         public ActionResult Login()
         {
